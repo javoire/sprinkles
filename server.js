8384950ad5d2048ac5632a6c4a9a402b6ee3f636
Olle Lundberg <geek@nerd.sh>
2015-08-11 14:29:37 +0200

    Knark is najs

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-var express    = require('express');
-var app        = express();
+var express = require('express');
+var app = express();
 var bodyParser = require('body-parser');
 var LastfmAPI = require('lastfmapi');
-var echojs    = require('echojs');
+var echojs = require('echojs');
+var Q = require('q');
 
 // server our public shitz
 app.use(express.static('public'));
 
 var lfm = new LastfmAPI({
-  'api_key' : process.env.LFM_KEY,
-  'secret' : process.env.LFM_SECRET
+  'api_key': process.env.LFM_KEY,
+  'secret': process.env.LFM_SECRET
 });
 
 var echo = echojs({
@@ -18,21 +19,48 @@ var echo = echojs({
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8880;        // set our port
 
 var router = express.Router();
 
-router.get('/:user', function(req, res) {
-  lfm.user.getTopArtists({"user": req.params.user, "period": "overall" },
-    function (err, artists) {
-    if (err) { return console.log('We\'re in trouble', err); }
-
-    res.json(artists);
+router.get('/:user', function (req, res) {
+  lfm.user.getTopArtists({'user': req.params.user, 'period': 'overall', 'limit': 50}, function (err, artists) {
+    var promises = [];
+    if (err) {
+      return console.log('Falied: ' + err);
+    }
+    artists['artist'].forEach(function (artist) {
+      var name = artist.name;
+      console.log("Querying: " + name);
+      var d = Q.defer();
+      promises.push(d.promise);
+      echo('artist/profile').get({'name': name, 'bucket': 'artist_location'},
+        function (err, json) {
+          var timer = setTimeout(d.resolve, 1000);
+          if (err) {
+            console.log('Echo failed for artist: ' + name);
+            console.log(json);
+            clearTimeout(timer);
+            d.resolve();
+          }
+          else if (json.response['artist']['artist_location'] && json.response['artist']['artist_location']['country']) {
+            console.log(name + ' -> ' + json.response['artist']['artist_location']['country']);
+            clearTimeout(timer);
+            d.resolve({artist: name, country: json.response['artist']['artist_location']['country']});
+          }
+        }
+      )
+    });
+    Q.all(promises).then(function (artists) {
+      return res.json(artists.filter(function (artist) {
+        return artist
+      }));
     });
   });
+});
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");

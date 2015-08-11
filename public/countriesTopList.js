afc797af932c1fd057e316363334a60a6100979f
Jonatan Dahl <jd@spotify.com>
2015-08-11 16:30:07 +0200

    mvp list over users top countries

--- public/countriesTopList.js
+++ public/countriesTopList.js
@@ -1,3 +1,19 @@
 function countriesTopList(countries) {
+  countries.sort(function(a, b) {
+    if (a.percent > b.percent) {
+      return -1;
+    }
+    if (a.percent < b.percent) {
+      return 1;
+    }
+    return 0;
+  });
 
+  var countryList = document.querySelector('#country-list ul');
+  countryList.innerHTML = '';
+  countries.forEach(function(country){
+    var li = document.createElement('li');
+    li.innerHTML = country.country + ' ' + Math.round(country.percent * 100) + '%';
+    countryList.appendChild(li);
+  })
 }

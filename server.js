d9f12b456b44431e53aa5ea28a818754229bd71d
Olle Lundberg <geek@nerd.sh>
2015-08-11 12:22:25 +0200

    Fix stuff

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ var express    = require('express');
 var app        = express();
 var bodyParser = require('body-parser');
 var LastfmAPI = require('lastfmapi');
+var echojs    = require('echojs');
 
 var lfm = new LastfmAPI({
   'api_key' : process.env.LFM_KEY,
   'secret' : process.env.LFM_SECRET
+});
+
+var echo = echojs({
+  key: process.env.ECHONEST_KEY
 });
 
 // configure app to use bodyParser()
@@ -17,7 +22,7 @@ var port = process.env.PORT || 8080;        // set our port
 
 var router = express.Router();
 
-router.get('/users/:user', function(req, res) {
+router.get('/:user', function(req, res) {
   lfm.user.getTopArtists({"user": req.params.user, "period": "overall" },
     function (err, artists) {
     if (err) { return console.log('We\'re in trouble', err); }

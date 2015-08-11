83f7b580c91e93644ffcfc43b1def1822ff6456e
Olle Lundberg <geek@nerd.sh>
2015-08-11 20:52:33 +0200

    Better textures.

--- client/renderGlobe.js
+++ client/renderGlobe.js
@@ -6,39 +6,34 @@ function renderUserDataOnGlobe(userCountryData, worldData) {
 
   var geo = geodecoder(countries.features);
 
-  // get the geojson for some countries
-  var someCountriesGeojson = { features: [], type: "FeatureCollection" };
-  userCountryData.forEach(function(country) {
+
+  var baseRed = 46;
+  var baseGreen = 189;
+  var baseBlue = 89;
+  var textures = [];
+  var rgb = d3.rgb(baseRed, baseGreen, baseBlue);
+  userCountryData.sort(function(a, b) {
+    return b.percent - a.percent;
+  });
+  userCountryData.forEach(function (country) {
     var countryGeojson = geo.find(country.country);
     if (countryGeojson) {
-      someCountriesGeojson.features.push(countryGeojson)
+      console.log(rgb);
+      textures.push(mapTexture({features: [countryGeojson], type: "FeatureCollection"}, rgb, 'black'));
+      rgb = rgb.darker();
     }
   });
 
-    // get the geojson for some countries
-    var baseRed = 46;
-    var baseGreen = 189;
-    var baseBlue = 89;
-    var textures = [];
-    userCountryData.forEach(function(country) {
-      var countryGeojson = geo.find(country.country);
-      if (countryGeojson) {
-        var rgb =  'rgb('+ baseRed +',' + baseGreen +','+baseBlue+')';
-        console.log(rgb);
-        textures.push(mapTexture({ features: [countryGeojson], type: "FeatureCollection" }, rgb, 'black'));
-      }
-    });
-
-    // google stuff
-    var map = document.getElementById('map');
-    var globe = new DAT.Globe(map, {
-      myCoolTexture: mapTexture(countries, '#222', 'black'),
-      overlayTextures: textures
-    });
-    console.log(globe);
-    var titlebar = document.querySelector('#country-list h2');
-    titlebar.appendChild(document.createTextNode('These are your top countries, ' + nick));
-    globe.animate();
+  // google stuff
+  var map = document.getElementById('map');
+  var globe = new DAT.Globe(map, {
+    myCoolTexture: mapTexture(countries, '#222', 'black'),
+    overlayTextures: textures
+  });
+  console.log(globe);
+  var titlebar = document.querySelector('#country-list h2');
+  titlebar.appendChild(document.createTextNode('These are your top countries, ' + nick));
+  globe.animate();
 
   document.body.style.backgroundImage = 'none'; // remove loading
 }

795f2ed351d332e395029b44fe2c626f14add4a8
Jonatan Dahl <jd@spotify.com>
2015-08-11 12:08:13 +0200

    tabs -> spaces

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ var bodyParser = require('body-parser');
 var LastfmAPI = require('lastfmapi');
 
 var lfm = new LastfmAPI({
-		'api_key' : process.env.LFM_KEY,
-		'secret' : process.env.LFM_SECRET
-		});
+  'api_key' : process.env.LFM_KEY,
+  'secret' : process.env.LFM_SECRET
+});
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -18,13 +18,13 @@ var port = process.env.PORT || 8080;        // set our port
 var router = express.Router();
 
 router.get('/users/:user', function(req, res) {
-		lfm.user.getTopArtists({"user": req.params.user, "period": "overall" },
-			function (err, artists) {
-			if (err) { return console.log('We\'re in trouble', err); }
+  lfm.user.getTopArtists({"user": req.params.user, "period": "overall" },
+    function (err, artists) {
+    if (err) { return console.log('We\'re in trouble', err); }
 
-			res.json(artists);
-			});
-		});
+    res.json(artists);
+    });
+  });
 
 
 app.use('/api', router);

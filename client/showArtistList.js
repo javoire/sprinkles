d7228c629f7bafe11671d6dd4708a3c9b23783c8
Jonatan Dahl <jd@spotify.com>
2015-08-12 11:52:57 +0200

    country and artist list restyle

--- client/showArtistList.js
+++ client/showArtistList.js
@@ -1,3 +1,18 @@
 function showArtistList() {
-  console.log('showArtistList');
+  var artistListWrapper = document.getElementById('artist-list');
+  var artistListHeader = document.querySelector('#artist-list h2');
+  var artistList = document.querySelector('#artist-list ul');
+  artistList.innerHTML = '';
+  countries.forEach(function(country){
+    var li = document.createElement('li');
+    li.innerHTML = country.country + ' ' + Math.round(country.percent * 100) + '%';
+
+    li.addEventListener('click', function() {
+      console.log('asdsadas');
+    })
+
+    artistList.appendChild(li);
+  });
+
+  artistListWrapper.className = 'show';
 }

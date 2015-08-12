a4cb9ed8ec836381b7cafc38b39357a5513a6b7d
Niklas Ek <nikek@spotify.com>
2015-08-12 11:21:21 +0200

    hovering robot country madnesszs

--- client/countriesTopList.js
+++ client/countriesTopList.js
@@ -2,13 +2,22 @@ function countriesTopList(countries) {
   var countryListWrapper = document.getElementById('country-list');
   var countryList = document.querySelector('#country-list ul');
   countryList.innerHTML = '';
+
+  // keeping track of the active list element for removing the active class form it
+  var activeItem = null;
+
+  // Fill with countries
   countries.forEach(function(country){
     var li = document.createElement('li');
     li.innerHTML = country.country + ' ' + Math.round(country.percent * 100) + '%';
 
     li.addEventListener('click', function() {
-      console.log('asdsadas');
-    })
+      if(activeItem) activeItem.classList.remove('active');
+
+      activeItem = li;
+      li.classList.add('active');
+      showArtistList(country);
+    });
 
     countryList.appendChild(li);
   });
